f5a7fc4cc42e740f0b2012adac30b6172e117e86
suntianjing <65645515+suntianjing@users.noreply.github.com>
2021-08-01 00:08:51 +0800

    2021.8.1 0:08

---
+++ 第二周作业/work3/mobile/src/components/MyHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyHeader"
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- 第二周作业/work3/vant-learn/src/main.js
+++ 第二周作业/work3/vant-learn/src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vant from 'vant';
 import 'vant/lib/index.css';
+import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(Vant);
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
